ee45314d8dad0d370326581f13d4396b96b5a5f3
karizmo <32525686+karizmo@users.noreply.github.com>
2018-12-06 10:01:10 -0800

    included contact and tips php files
    update

---
+++ mysql/AddContactInfo.php
@@ -0,0 +1,26 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+//var_dump($_POST);
+//exit;
+
+try {
+    $conn = new PDO("mysql:host=a07yd3a6okcidwap.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=p3slyityqr9arsx2", "gupazml2ebchali0", "ng7ehrbl1adzi9q0");
+}
+//p3slyityqr9arsx2 dabatase
+catch(PDOException $e)
+{
+    echo "Error: " .$e->getMessage();
+}
+$uId =$_POST['userId'];
+$newHomeInfo=$_POST['homeInfo'];
+
+//$query = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+
+$query = "UPDATE users SET address='$newHomeInfo' WHERE id = '$uId';";
+$result = $conn->query($query);
+if($result){
+  echo json_encode(true);
+} else {
+  echo json_encode (false);
+}
+?>
